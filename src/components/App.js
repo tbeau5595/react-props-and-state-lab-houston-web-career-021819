0e3ee86e72ac53793ae6532a6fd716a1c2687243
github username <github email address>
2019-04-24 16:50:20 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,57 @@ import PetBrowser from './PetBrowser'
 class App extends React.Component {
   constructor() {
     super()
+    this.onChangeType('all')
 
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
+    }
+  }
+
+  adopt = (id) => {
+    this.setState({
+      pets: this.state.pets.map(pet => {
+        if(pet.id == id){
+          return {
+            ...pet, isAdopted: !pet.isAdopted
+          }
+        } else {
+          return pet
+        }
+      })
+    })
+  }
+
+  onChangeType = (value) => {
+    if(value === 'all'){
+      fetch('/api/pets')
+      .then((res)=>{
+        return res.json()
+      })
+      .then((pets)=>{
+        return this.setState ({
+          pets: pets,
+          filters: {
+            type: value
+          }
+        })
+      })
+    } else {
+      fetch(`/api/pets?type=${value}`)
+      .then((res)=>{
+        return res.json()
+      })
+      .then((pets)=>{
+        return this.setState ({
+          pets: pets,
+          filters: {
+            type: value
+          }
+        })
+      })
     }
   }
 
@@ -24,10 +69,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser adopt={this.adopt} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,7 +6,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select  onChange={(e) => this.props.onChangeType(e.target.value)} name="type" id="type">
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
